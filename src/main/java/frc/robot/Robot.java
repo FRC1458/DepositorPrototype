3f76331d6a4be5a49fe2b6f6a26a2b149755c88f
Skizmo42 <eman06040@icloud.com>
2021-07-22 13:27:24 -0700

    added more talon motors and possibly the right id numbers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,30 +12,65 @@ import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.XboxController.Axis;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  private final WPI_TalonSRX leftMotor = new WPI_TalonSRX(RobotConstants.leftMotorID);
-  private final WPI_TalonSRX rightMotor = new WPI_TalonSRX(RobotConstants.rightMotorID);
-  private final WPI_TalonSRX leftMotor2 = new WPI_TalonSRX(RobotConstants.leftMotor2ID);
-  private final WPI_TalonSRX rightMotor2 = new WPI_TalonSRX(RobotConstants.rightMotor2ID);
-  private final DifferentialDrive robotDrive = new DifferentialDrive(leftMotor, rightMotor);
-  //private final Servo servo = new Servo(0);
-  //private final Solenoid solenoid = new Solenoid(0);
-  private final WPI_TalonSRX quantumIntake = new WPI_TalonSRX(RobotConstants.quantumIntakeID);
+  private  WPI_TalonSRX leftMotor;
+  private  WPI_TalonSRX rightMotor;
+  private  WPI_TalonSRX leftMotor2;
+  private  WPI_TalonSRX rightMotor2;
 
-  private final Joystick stick = new Joystick(0);
-  private final XboxController controller = new XboxController(0);
+  private  WPI_TalonSRX elevatorMotorUp;
+  private  WPI_TalonSRX elevatorMotorDown;
+  private  WPI_TalonSRX feederMotor;
 
+  private  DifferentialDrive robotDrive;
+  // private Servo servo;
+  // private Solenoid solenoid;
+  private WPI_TalonSRX quantumIntake;
+
+  private Joystick leftStick;
+  private Joystick rightStick;
+  private XboxController controller;
+  //private Hand hand;
+  //private Hand kRight;
+
+  public Robot() {
+    super(0.03);
+    leftMotor = new WPI_TalonSRX(RobotConstants.leftMotorID);
+    rightMotor = new WPI_TalonSRX(RobotConstants.rightMotorID);
+    leftMotor2 = new WPI_TalonSRX(RobotConstants.leftMotor2ID);
+    rightMotor2 = new WPI_TalonSRX(RobotConstants.rightMotor2ID);
+
+    elevatorMotorUp = new WPI_TalonSRX(RobotConstants.elevatorMotorUpID);
+    //elevatorMotorDown = new WPI_TalonSRX(RobotConstants.elevatorMotorDownID);
+    feederMotor = new WPI_TalonSRX(RobotConstants.feederMotorID);
+
+    robotDrive = new DifferentialDrive(leftMotor, rightMotor);
+    //servo = new Servo(0);
+    //solenoid = new Solenoid(0);
+    quantumIntake = new WPI_TalonSRX(RobotConstants.quantumIntakeID);
+
+    leftStick = new Joystick(0);
+    rightStick = new Joystick(1);
+    controller = new XboxController(0);
+    //hand = k;
+    //kRight = new Hand();
+  }
+
+  @Override
   public void robotInit() {
     leftMotor.setNeutralMode(NeutralMode.Brake);
     rightMotor.setNeutralMode(NeutralMode.Brake);
@@ -46,9 +81,10 @@ public class Robot extends TimedRobot {
     rightMotor2.follow(rightMotor);
   }
 
+  @Override
   public void teleopInit() {
-    //servo.setAngle(0);
-    //solenoid.set(false);
+    // servo.setAngle(0);
+    // solenoid.set(false);
   }
 
   @Override
@@ -56,12 +92,33 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    robotDrive.arcadeDrive(stick.getY(), -stick.getX());
-    if(controller.getAButton()){
+    //robotDrive.arcadeDrive(leftStick.getY(), -leftStick.getX());
+
+    robotDrive.tankDrive(controller.getTriggerAxis(Hand.kLeft), controller.getTriggerAxis(Hand.kRight));
+    if(controller.getBumper(Hand.kLeft)){
       quantumIntake.set(.5);
     }
-    else if(controller.getBButton()){
-      quantumIntake.set(-.5);
+    else {
+      quantumIntake.set(0);
+    }
+
+    if(controller.getBumper(Hand.kRight)) {
+      feederMotor.set(.5);
+    }
+    else {
+      feederMotor.set(0);
+    }
+    if (leftStick.getY()>0) {
+      elevatorMotorUp.set(1);
+      elevatorMotorDown.set(0);
+    }
+    else if (leftStick.getY()<0) {
+      //elevatorMotorDown.set(1);
+      elevatorMotorUp.set(0);
+    }
+    else {
+      //elevatorMotorDown.set(0);
+      elevatorMotorUp.set(0);
     }
   }
 }
--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -1,10 +1,13 @@
 package frc.robot;
 
 public class RobotConstants {
-    public final static int leftMotorID = 3;
-    public final static int rightMotorID = 2;
-    public final static int leftMotor2ID = 1;
-    public final static int rightMotor2ID = 4;
-    public final static int quantumIntakeID = 5;
+    public final static int leftMotorID = 13;
+    public final static int rightMotorID = 20;
+    public final static int leftMotor2ID = 11;
+    public final static int rightMotor2ID = 17;
+    public final static int quantumIntakeID = 23;
+    public final static int elevatorMotorUpID = 19;
+    //public final static int elevatorMotorDownID = 0;
+    public final static int feederMotorID = 18;
     //public final static int armMotorID = 5;
 }

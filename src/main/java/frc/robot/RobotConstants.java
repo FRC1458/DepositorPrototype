ef88bef6b9f85d4fd65999046d08da68aee5f800
Skizmo42 <eman06040@icloud.com>
2022-01-15 14:00:34 -0800

    commit again

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -1,16 +1,6 @@
 package frc.robot;
 
 public class RobotConstants {
-    public final static int leftMotorID = 13;
-    public final static int rightMotorID = 20;
-    public final static int leftMotor2ID = 11;
-    public final static int rightMotor2ID = 17;
-    //public final static int quantumIntakeID = 23;
-    public final static int elevatorMotorUpID = 19;
-    //public final static int elevatorMotorDownID = 0;
-    public final static int feederMotorID = 18;
-    //public final static int armMotorID = 5;
-    public final static int shooterID = 1;
-    public final static int pingID = 0;
-    public final static int echoID = 0;
+    public final static int leftMotorID = 2;
+    public final static int rightMotorID = 3;
 }
